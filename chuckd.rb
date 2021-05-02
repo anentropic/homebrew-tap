84f40da07b2b44c3a0ede865aeb64932b77fd767
anentropic <ego@anentropic.com>
2021-05-02 19:27:19 +0100

    Update chuckd to 0.3.7

--- chuckd.rb
+++ chuckd.rb
@@ -3,8 +3,8 @@ require_relative "./require/graalvm"
 class Chuckd < Formula
   desc "Schema evolutions validator"
   homepage "https://github.com/anentropic/chuckd"
-  url "https://github.com/anentropic/chuckd.git"
-  version "0.3.6"
+  url "https://github.com/anentropic/chuckd.git", tag: "0.3.7", revision: "6a664f2ddee6e19ed45a9508e5bf4b9a269c46c7"
+  version "0.3.7"
   sha256 "68e4deea7f41602f2d4e63ff06bb7fbb1c7e269c6cfa8a43bb0e47aabe6a8774"
   license "Apache-2.0"
 
