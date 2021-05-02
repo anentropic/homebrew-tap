73c8b483bba27a26f854132aac437f40efcc7840
Anentropic <ego@anentropic.com>
2021-05-02 20:11:59 +0100

    linting

--- chuckd.rb
+++ chuckd.rb
@@ -5,12 +5,11 @@ class Chuckd < Formula
   homepage "https://github.com/anentropic/chuckd"
   url "https://github.com/anentropic/chuckd.git", tag: "0.3.8", revision: "8bf166cc75d8980866473513142b716222e15fc2"
 
-bottle do
+  bottle do
     root_url "https://github.com/anentropic/chuckd/releases/download/0.3.8"
     
     sha256 cellar: :any, catalina: "30fba23ff485e2b129b3c24a6d8633600f606a2f90d2f4ba01f58b04bea522f0"
   end
-
 
   sha256 "68e4deea7f41602f2d4e63ff06bb7fbb1c7e269c6cfa8a43bb0e47aabe6a8774"
   license "Apache-2.0"
