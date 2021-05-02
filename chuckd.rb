953dcc24a115e43406f5925cb8b0cef0d4748695
Anentropic <ego@anentropic.com>
2021-05-02 19:57:43 +0100

    remove unnecessary version key

--- chuckd.rb
+++ chuckd.rb
@@ -5,9 +5,6 @@ class Chuckd < Formula
   homepage "https://github.com/anentropic/chuckd"
   url "https://github.com/anentropic/chuckd.git", tag: "0.3.8", revision: "8bf166cc75d8980866473513142b716222e15fc2"
 
-
-
-  version "0.3.8"
   sha256 "68e4deea7f41602f2d4e63ff06bb7fbb1c7e269c6cfa8a43bb0e47aabe6a8774"
   license "Apache-2.0"
 
