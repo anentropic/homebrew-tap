45bcdbf85fe1df6ea758d6454b040b9f214569af
anentropic <ego@anentropic.com>
2021-05-03 11:48:25 +0100

    Add bottle for chuckd 0.3.15 on macOS Catalina

--- chuckd.rb
+++ chuckd.rb
@@ -5,6 +5,12 @@ class Chuckd < Formula
   homepage "https://github.com/anentropic/chuckd"
   url "https://github.com/anentropic/chuckd.git", tag: "0.3.15", revision: "0e698b421e50dce776275501d9c40a4afb11ffa4"
 
+  bottle do
+    root_url "https://github.com/anentropic/chuckd/releases/download/0.3.15"
+
+    sha256 cellar: :any, catalina: "dff988b47ec4691f57f147dbdf905ed0286723b125928873c3a7fb26e3f34ae8"
+  end
+
   license "Apache-2.0"
 
   depends_on "gradle" => :build
