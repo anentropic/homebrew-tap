b062d90f1be0da0883a4aa7edfeaea9cc6847593
anentropic <ego@anentropic.com>
2021-05-03 11:22:32 +0100

    Update chuckd to 0.3.14

--- chuckd.rb
+++ chuckd.rb
@@ -4,6 +4,13 @@ class Chuckd < Formula
   desc "Schema evolutions validator"
   homepage "https://github.com/anentropic/chuckd"
   url "https://github.com/anentropic/chuckd.git", tag: "0.3.14", revision: "25fa980295c7c61698994b97dc223a6b926e9edd"
+
+  bottle do
+    root_url "https://github.com/anentropic/chuckd/releases/download/0.3.14"
+
+    sha256 cellar: :any, catalina: "2b2204f39dd2698de4036634e35c963f4ee85e0748ffca86a9918154c069b90a"
+  end
+
   license "Apache-2.0"
 
   depends_on "gradle" => :build
