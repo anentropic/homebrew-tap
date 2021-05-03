30f48ed64542ca3dec2534dce54243bb739aba9d
anentropic <ego@anentropic.com>
2021-05-03 13:11:06 +0100

    Update chuckd to 0.3.18

--- chuckd.rb
+++ chuckd.rb
@@ -3,13 +3,7 @@ require_relative "./require/graalvm"
 class Chuckd < Formula
   desc "Schema evolutions validator"
   homepage "https://github.com/anentropic/chuckd"
-  url "https://github.com/anentropic/chuckd.git", tag: "0.3.17", revision: "1b1deff89f24a53c4a932cedd7d234113398d29f"
-
-  bottle do
-    root_url "https://github.com/anentropic/chuckd/releases/download/0.3.17"
-
-    sha256 cellar: :any, catalina: "d0e55f94db69bbea0dfcedaf51cf573bc268b8bd9deda100eaa28cd05c5bc95f"
-  end
+  url "https://github.com/anentropic/chuckd.git", tag: "0.3.18", revision: "2dfc05010b18b91c3809b485b7f42890eed30879"
 
   license "Apache-2.0"
 
