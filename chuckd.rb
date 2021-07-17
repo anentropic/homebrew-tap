9f001748e891cf0a24b78ba312227d842104d11b
anentropic <ego@anentropic.com>
2021-07-17 17:24:39 +0100

    Update chuckd to 0.4.0

--- chuckd.rb
+++ chuckd.rb
@@ -3,15 +3,9 @@ require_relative "./require/graalvm"
 class Chuckd < Formula
   desc "Schema evolutions validator"
   homepage "https://github.com/anentropic/chuckd"
-  url "https://github.com/anentropic/chuckd.git", tag: "0.3.19", revision: "ee38272baa1fdd1bbbe71d09da5b576b126a2eca"
+  url "https://github.com/anentropic/chuckd.git", tag: "0.4.0", revision: "07a631bad482203ba674f3cb26495f8bc6b9f8c9"
 
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/anentropic/chuckd/releases/download/0.3.19"
-
-    sha256 cellar: :any, catalina: "d4c76d961664bf6c24ae445b6f5516355dd4c1ffcff562a57767f50bdf19e3ae"
-  end
 
   depends_on GraalVMRequirement => :build
   depends_on "gradle" => :build
