a964e1c36a4e157bf911f97bd15a5cc1ec011c44
Paul Garner <ego@anentropic.com>
2022-12-23 10:09:03 +0000

    Update chuckd to 0.5.3

--- chuckd.rb
+++ chuckd.rb
@@ -3,7 +3,7 @@ require_relative "./require/graalvm"
 class Chuckd < Formula
   desc "Schema evolutions validator"
   homepage "https://github.com/anentropic/chuckd"
-  url "https://github.com/anentropic/chuckd.git", tag: "0.5.3", revision: "6ff936dacab6accf584204b8c26070f3aec6d955"
+  url "https://github.com/anentropic/chuckd.git", tag: "0.5.3", revision: "684020c3a0b2d34350c5c81f5b3e46174eb0b381"
 
   license "Apache-2.0"
 
