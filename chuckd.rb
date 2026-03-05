5e2d6507c0f808e724cc5cfce0830f8fed26a3b2
Paul Garner <ego@anentropic.com>
2026-03-05 17:52:20 +0000

    Update chuckd to 0.5.3

--- chuckd.rb
+++ chuckd.rb
@@ -1,7 +1,7 @@
 class Chuckd < Formula
   desc "Schema evolutions validation tool, for JSON Schema, Protobuf and Avro"
   homepage "https://github.com/anentropic/chuckd/"
-  url "https://github.com/anentropic/chuckd.git", tag: "0.5.4", revision: "cdaac6365b89cdf1fbbfb3a0450814f031b75922"
+  url "https://github.com/anentropic/chuckd.git", tag: "0.5.3", revision: "684020c3a0b2d34350c5c81f5b3e46174eb0b381"
   license "Apache-2.0"
 
   depends_on "gradle" => :build
@@ -11,7 +11,7 @@ class Chuckd < Formula
   def install
     system "gradle", "--no-daemon", "-Dorg.gradle.java.home=#{HOMEBREW_PREFIX}/opt/openjdk/", "build"
 
-    system "unzip", "app/build/distributions/app-shadow-#{version "0.5.4"}.zip"
+    system "unzip", "app/build/distributions/app-shadow-#{version "0.5.3"}.zip"
     libexec.install Dir["app-shadow-#{version}/lib/*"]
     write_non_overridable_jar_script bin, libexec/"chuckd-#{version}-all.jar", "chuckd"
 
